7bf0b7fa0c7692d45b898e8bdf600432f93a7be9
maikoo <mikey.chan1@gmail.com>
2018-06-01 19:49:37 -0700

    Snippet finder now handles syntax highlighted code, display all and filter functionality.

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Create Update Delete App
 Goals:
 - [x] Input form with single text input
 - [x] click handler to capture text data form field
-- [ ] helper utilities to read/write local storage
+- [x] helper utilities to read/write local storage
+- [x] Handles code (text area instead of text)
+- [x] Two fields for label and snippet
+- [x] Display all functionality
+- [x] Search by name
--- app.js
+++ app.js
@@ -1,21 +1,61 @@
 $(document).ready(function () {
-    // $('body').html(<p>Hello World</p>)
+    // let dataObj = {}
+
     $('.setData').on('click', function () {
-        let textFieldValue = $('.textField').val()
-        // console.log(textFieldValue)
-        $('.debug').text(textFieldValue)
-        localStorage.setItem('myFormTextData', textFieldValue)
-        $('.textField').val('')
+        let shortName = $('.shortName').val()
+        let textAreaVal = $('.textArea').val()
+        let textAreaCode = `<pre><code>${textAreaVal}</code></pre>`
+        // $('.debug').html(`<pre><div class= snip ${shortName}>${shortName}</div></br><code>${textAreaVal}</code></pre>`)
+        localStorage.setItem(`${shortName}`, textAreaCode)
+        $('.textArea').val('')
+        $('.shortName').val('')
     })
 
-    $('.getData').on('click', function () {
-        let retrieveData = localStorage.getItem('myFormTextData')
-        $('.debug').text(retrieveData)
+
+    $('.getAllData').on('click', function () {
+        // let retrieveData = localStorage.getItem('myFormTextData')
+        // $('.debug').html(`<pre>;&lt;code&gt;${retrieveData}&lt;/code&gt;</pre>`)
+        $('.snippetList').html('')
+        for(var key in localStorage) {
+            console.log(key)
+            if(typeof localStorage[key] === 'string') {
+                // let textCode = $('.snippetList').val().replace(/\n/g, '<br>').replace(/ /g, '&nbsp');
+                // $('.snippetList').append(`<div class=${key}><pre><code>${localStorage[key]}</code></pre></div>`)
+                $(`<div class="snip ${key}"><pre><code>${localStorage[key]}</code></pre></div>`).appendTo('.snippetList')
+
+                $('.snip').each(function (i, block) {
+                    hljs.highlightBlock(block);
+                });
+            } 
+        }
+
     })
+    $('.filter').on('keypress', function () {
+        let shortName = $('.shortName').val()
+        let textAreaVal = $('.textArea').val()
+        let textAreaCode = `<pre><code>${textAreaVal}</code></pre>`
+        let filter = $('.filter').val()
+        console.log(filter)
+        // $('.snippetList').html(`<pre><div class= snip ${shortName}>${shortName}</div></br><code>${textAreaVal}</code></pre>`)
+        for(var key in localStorage) {
+            if( filter === key) {
+                $('.snippetList').html('')
+                $(`<div class="snip ${key}"><pre><code>${localStorage[key]}</code></pre></div>`).appendTo('.snippetList')
+
+                $('.snip').each(function (i, block) {
+                    hljs.highlightBlock(block);
+                });
+            }
+            // console.log(key)
+
+        }
+    })
+
+
 
     // $('.textField').on('keyup', function () {
     //     let textFieldValue = $('.textField').val()
     //     // console.log(textFieldValue)
-    //     $('.debug').text(textFieldValue)
+    //     $('.snippetList').text(textFieldValue)
     // })
 })
